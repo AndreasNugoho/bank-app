9e13cac84d4b882194b752daa86397f03f4b1405
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-08-27 13:37:58 +0700

    feat:format amount"

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
@@ -12,6 +13,12 @@ const Home = () => {
             title="Welcome"
             user={loggedIn?.firstName || 'Guest'}
             subtext="Access and message your account and transactions" 
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+            
           />
         </header>
       </div>
