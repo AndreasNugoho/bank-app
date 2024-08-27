1418a42e3849a1dcf255afff5756b7f87ec92ce7
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-08-27 13:19:13 +0700

    feat:home page up" q quit /q \q

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,18 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: "Andre" };
   return (
     <section className="home">
       <div className="home-content">
         <header className='home-header'>
-          Welcome, Andre
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and message your account and transactions" 
+          />
         </header>
       </div>
     </section>
