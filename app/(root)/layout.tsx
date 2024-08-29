574e15574ea998e4848916e235b01eff42eec25a
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-08-29 08:53:40 +0700

    sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,15 @@
+import Sidebar from "@/components/Sidebar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+    const loggedIn = {firstName:'Andre',lastName:'Nugroho'}
     return (
-        <main>
-            SIDEBAR    
+        <main className="flex h-screen w-full font-inter">
+        <Sidebar user={ loggedIn} />
             {children}
         </main>
   );
