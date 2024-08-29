aa5ca55a282d11b97966980269ee16269ab0a817
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-08-29 09:40:43 +0700

    fix:sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,14 @@
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
 import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
 
-const Sidebar = ({user}:SiderbarProps) => {
+const Sidebar = ({ user }: SiderbarProps) => {
+    const pathname = usePathname();
   return (
     <section className="sidebar">
         <nav className="flex flex-col gap-4">
@@ -21,9 +26,16 @@ const Sidebar = ({user}:SiderbarProps) => {
                     Bank-Ku
                   </h1>
               </Link>
-              {sidebarLinks.map((item)=> {
+              {sidebarLinks.map((item) => {
+                  const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
                   return (
-                      <Link href={item.route} key={item.label}>
+                      <Link
+                          href={item.route}
+                          key={item.label}
+                          className={cn('sidebar-link', {
+                              'bg-bank-gradient': isActive
+                          })}
+                      >
                         {item.label}
                       </Link>
                 )
