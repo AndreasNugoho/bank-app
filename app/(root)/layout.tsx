6ccd11aee36bf01b706bb20ef889bc660da90ab8
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-08-29 17:57:40 +0700

    sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -11,25 +11,17 @@ export default function RootLayout({
     const loggedIn = {firstName:'Andre',lastName:'Nugroho'}
     return (
         <main className="flex h-screen w-full font-inter">
-        <Sidebar user={loggedIn} />
-          
+          <Sidebar user={loggedIn} />
+
           <div className="flex size-full flex-col">
-            <div className="root-layout"> 
-            <Image
-              src="/icons/logo.svg"
-              width={30}
-              height={30}
-              alt="menu-icon"
-            />
-            <div>
-              <MobileNav
-                user={loggedIn}
-                
-              />
+            <div className="root-layout">
+              <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+              <div>
+                <MobileNav user={loggedIn} />
+              </div>
             </div>
-            </div>
-          </div>
             {children}
+          </div>
         </main>
   );
 }
