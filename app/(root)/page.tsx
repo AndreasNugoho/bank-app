22ef1ec4b26d6159c7bea4f1899da2038f7f8cd8
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-09-01 15:00:29 +0700

    feat:right side bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
@@ -24,6 +25,12 @@ const Home = () => {
 
         RECENT TRANSACTION
       </div>
+
+      <RightSideBar
+        user={loggedIn}
+        transactions={[]}
+        banks={[]}
+      />
     </section>
   )
 }
--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 
-const RightSideBar = () => {
+const RightSideBar = ({user,transactions,banks}:RightSidebarProps) => {
   return (
-    <div>
-      
-    </div>
+    <aside className="right-sidebar">
+      <section className="flex flex-col pb-8">
+        <div className="profile-banner" />
+        <div className="profile">
+          <div className="profile-img">
+            <span className="text-5xl font-bold text-blue-500">{user.firstName[0]}</span>
+          </div>
+
+          <div className="profile-details">
+            <h1>
+              {user.firstName} {user.lastName}
+            </h1>
+          </div>
+        </div>
+      </section>
+    </aside>
   )
 }
 
