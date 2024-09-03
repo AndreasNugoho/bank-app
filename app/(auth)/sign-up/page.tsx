2d9aaa4a77f2a20b1da82748f8b492c524869473
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-09-03 20:55:49 +0700

    feat:auth form

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,11 +1,12 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
-const SignUp = () => {
+const SignUn = () => {
   return (
-    <div>
-        SignUp
+    <div className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up" />
     </div>
   )
 }
 
-export default SignUp
+export default SignUn
