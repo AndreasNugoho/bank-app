6c81476c2c5ad50b93b9a11beebe3903aae27d4c
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-09-14 14:58:40 +0700

    fix form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,6 +18,7 @@ import {
 import { Input } from "@/components/ui/input"
 import CustomInput from './CustomInput'
 import { authFormSchema } from '@/lib/utils'
+import { Loader2 } from 'lucide-react'
  
 const formSchema = z.object({
     email: z.string().email(),
@@ -25,6 +26,7 @@ const formSchema = z.object({
 
 const AuthForm = ({ type }: { type: string }) => {
     const [user, setUser] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
     // 1. Define your form.
     const form = useForm<z.infer<typeof authFormSchema>>({
         resolver: zodResolver(authFormSchema),
@@ -38,7 +40,9 @@ const AuthForm = ({ type }: { type: string }) => {
     function onSubmit(values: z.infer<typeof authFormSchema>) {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
+        setIsLoading(true)
         console.log(values)
+        console.log(false)
     }
 
     return (
@@ -83,19 +87,36 @@ const AuthForm = ({ type }: { type: string }) => {
                         <Form {...form}>
                             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                                     <CustomInput 
-                                        control={form.control} name='email' label="Email" placeholder="Enter your email"
+                                        control={form.control} name='email' label="Email" placeholder="Enter your username"
                                     />
                                 
                                     <CustomInput 
                                         control={form.control} name='password' label="Password" placeholder="Enter your password"
                                     />
-
-                                    <CustomInput 
-                                        control={form.control} name='username' label="Password" placeholder="Enter your password"
-                                    />
-                                <Button type="submit">Submit</Button>
+                                
+                                <div className="flex flex-col gap-4">
+                                    <Button type="submit" disabled={isLoading} className="form-btn">
+                                        {isLoading ? (
+                                            <>
+                                                <Loader2 size={20}
+                                                className="animate-spin"
+                                                /> &nbsp; Loading...
+                                            </>
+                                        ): type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+                                    </Button>
+                                </div>
                             </form>
                         </Form>
+
+                        <footer className="flex justify-center gap-1">
+                            <p className="text-14 font-normal text-gray-600">{type === 'sign-in'
+                                ? "Dont have an account?"
+                                : "Already have an account?"
+                            }</p>
+                            <Link href={type === 'sign-in'? '/sign-up':'/sign-in'} className="form-link">
+                                {type === 'sign-in'? 'Sign Up':'Sign In'}
+                            </Link>
+                        </footer>
                     </>
             )}
         </section>
--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { FormControl, FormField, FormLabel, FormMessage } from './ui/form'
 import { Input } from './ui/input'
-import { Control, Form } from 'react-hook-form'
+import { Control, FieldPath, Form } from 'react-hook-form'
 import { z } from 'zod'
 import { authFormSchema } from '@/lib/utils'
 
 
 interface CustomInput { 
     control: Control<z.infer<typeof authFormSchema>>,
-    name: 'email' | 'password',
+    name: FieldPath<z.infer<typeof authFormSchema>>,
     label: string,
     placeholder: string
 }
@@ -26,7 +26,7 @@ const CustomInput = ({control,name,label,placeholder}:CustomInput) => {
                         <Input
                             placeholder={placeholder}
                             className="input-class"
-                            type="password"
+                            type={name === 'password' ? 'password' : 'text'}
                             {...field}
                         />
                     </FormControl>
