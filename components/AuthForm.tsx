399b964b15c79bf7f2b6dde8c893b5e89f10141b
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-09-14 15:22:52 +0700

    utils

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -93,13 +93,15 @@ const AuthForm = ({ type }: { type: string }) => {
                                         <CustomInput 
                                         control={form.control} name='lastName' label="Last Name" placeholder="Enter your last name"/>
                                         <CustomInput 
-                                        control={form.control} name='address' label="Address" placeholder="Enter your address"/>
+                                        control={form.control} name='address1' label="Address" placeholder="Enter your address"/>
                                         <CustomInput 
                                         control={form.control} name='state' label="State" placeholder="Example: NY"/>
                                         <CustomInput 
                                         control={form.control} name='postalCode' label="Postal Code" placeholder="Example: 11101"/>
                                         <CustomInput 
                                         control={form.control} name='dateOfBirth' label="Date Of Birth" placeholder="YYYY-MM-DD"/>
+                                        <CustomInput 
+                                        control={form.control} name='ssn' label="SSN" placeholder="Example: 1234"/>
                                     </>
                                 )}
                                     <CustomInput 
