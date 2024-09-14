e616def57f5754378774238d67ed6c28c0f89918
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-09-14 21:40:06 +0700

    user action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,6 +1,8 @@
 'use server';
 
-import { createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { ID } from "node-appwrite";
 
 export const signIn = async () => {
     try {
@@ -11,8 +13,23 @@ export const signIn = async () => {
 }
 
 export const signUp = async (userData: SignUpParams) => {
+    const { email, password, firstName, lastName } = userData;
     try {
-        
+        const {account} = await createAdminClient();
+        const newUserAccount = await account.create(
+            ID.unique(),
+            email,
+            password,
+            `${firstName} ${lastName}`
+        );
+        const session = await account.createSession(email, password);
+
+        cookies().set('appwrite-session', session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+        });
     } catch (error) {
         console.log('error', error);
     }
