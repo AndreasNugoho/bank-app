d10bdc5be5998dd63bc71b371865d256711142d4
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2024-09-15 12:26:51 +0700

    fix

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,9 @@
 import AuthForm from '@/components/AuthForm'
+import { getLoggedInUser } from '@/lib/actions/user.action';
 import React from 'react'
 
-const SignUn = () => {
+const SignUn = async() => {
+  const loggedInUser = await getLoggedInUser();
   return (
     <div className="flex-center size-full max-sm:px-6">
       <AuthForm type="sign-up" />
